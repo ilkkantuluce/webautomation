46ba2fe81fe390298dce36de9f8b01495709df71
ilkkantuluce07 <60572381+ilkkantuluce07@users.noreply.github.com>
2021-08-17 23:52:37 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -48,6 +48,14 @@ $("#in_app_extractors_new_session").change(function() {
 });
 
 
+
+
+$("#make-it-active").click(function(){
+  $(this).toggleClass("bg-success");
+  $("#make-it-active p").text($("#make-it-active p").text() == 'Active' ? 'Activate New One' : 'Active');
+});
+
+
 $('.datepicker').datepicker({
   inline: true
 });
